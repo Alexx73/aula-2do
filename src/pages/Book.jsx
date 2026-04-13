6ff5467830ef83c849ed201983a9274edadb462e
Alexx73 <tristanluna@yahoo.com.ar>
2026-04-13 11:28:45 -0300

    agregado, libro mejorado

--- src/pages/Book.jsx
+++ src/pages/Book.jsx
@@ -1,20 +1,34 @@
 import React from "react";
 
-import pdf from "../assets/english_plus_starter_unit_1_2_3.pdf";
-export default function Book()  {
+const pdfUrl =
+  "https://alexx73.github.io/aula-2do/assets/english_plus_starter_unit_1_2_3_new.pdf";
+
+const viewer = `https://docs.google.com/gview?url=${pdfUrl}&embedded=true`;
+
+export default function Book() {
   return (
     <div className="w-full h-[calc(100vh-80px)] flex flex-col">
 
-    
-     
-      {/* Visor PDF */}
+      <div className="flex justify-between items-center bg-gray-100 px-6 py-3 border-b">
+        <h1 className="text-lg font-bold">
+          📘 English Plus Starter - Units 1,2,3
+        </h1>
+
+        <a
+          href={pdfUrl}
+          download
+          className="bg-green-600 hover:bg-green-700 text-white px-4 py-2 rounded-lg"
+        >
+          ⬇ Descargar
+        </a>
+      </div>
+
       <iframe
-        src={pdf}
-        title="English Book"
+        src={viewer}
         className="flex-1 w-full"
+        title="Book"
       />
 
     </div>
   );
-};
-
+}
